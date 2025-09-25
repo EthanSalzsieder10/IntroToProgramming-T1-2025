03351a2cb3931c8192f543309aaed10c6ac8843d
Salzsieder Ethan <824658@stma.k12.mn.us>
2025-09-25 14:11:22 -0500

    2.4 start

---
+++ Practice/2.4practice.py
@@ -0,0 +1,9 @@
+# operands +, -, *, /
+print(5%3) #remainder of division
+print(3**2) #exponents
+print(10//3) #divides and rounds down
+
+first_name = "Ethan"
+last_name = "Salzsieder"
+age = 15
+print("Name: " + first_name + " " + last_name + ", Age: " + str(age))
