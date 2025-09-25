b2d1ab04fbe58b05f88be75ec1cb67d0d4870998
Salzsieder Ethan <824658@stma.k12.mn.us>
2025-09-25 13:11:06 -0500

    2.3-practice

---
+++ Practice/2.3practice.py
@@ -0,0 +1,20 @@
+print(2 + 2) #Integer
+print(2.4) # Float
+print('c') #Character
+print("Don't") #Single vs Double Quotes
+print(True) #Boolean
+X = 10 #Define a Variable
+print(X) # Variable
+x = 10 # Case Sensitive
+y = 50
+potato = 7
+print(x*y*potato)
+aaaaaaaaaaaaaaaaaa = 11 #prolly shouldn't name it this
+lowercase = False
+UPPERCASE = False
+camelCase = False
+UpperCamelCase = False
+kebab-case = False #doesn't even work
+snake_case = True
+SCREAMING_SNAKE_CASE = True
+# Keep Names Concise
