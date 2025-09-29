fb95f1e669f9c261072a6850e589da5c6613a6bd
Salzsieder Ethan <824658@stma.k12.mn.us>
2025-09-29 14:05:55 -0500

    simple mad lib start

---
+++ Assignments/simple_mad_lib.py
@@ -0,0 +1,40 @@
+'''
+Once upon a [noun], there lived a/n [adjective] princess.
+The princess had a [color] dress and was [number] feet tall.
+The princess spent her days locked in a [noun], and could only pass the time [verb ending in -ing]
+Legend said that the only one who could save her was her one true [noun]
+One [unit of time], a brave knight named [name] came for her
+The knight tried tosave the princess by [adverb] climbing the tower walls
+The knight, however, was too [adjective], and slid right down
+The princess realized she could just jump out and did so, only breaking her [noun]
+'''
+
+'''
+Pseudocode:
+input noun 1
+input adjective 1
+input color
+input number
+input noun 2
+input verb 1
+input noun 3
+input unit of time
+input name
+input adverb
+input adjective
+input noun 4
+print story
+'''
+
+noun_1 = input("noun\n> ")
+adjective_1 = input("adjective\n> ")
+color = input("color\n> ")
+number = input("number\n> ")
+noun_2 = input("noun\n> ")
+verb_1 = input("verb ending in -ing\n> ")
+noun_3 = input("noun\n> ")
+unit_time = input("unit of time\n> ")
+name = input("name\n> ")
+adverb = input("adverb\n> ")
+adjective_2 = input("adjective\n> ")
+noun_4 = input("noun\n> ")
