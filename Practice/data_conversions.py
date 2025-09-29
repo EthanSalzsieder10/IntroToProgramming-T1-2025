46e8d2f9a432de09f8acd92f50317dbafcb8245c
Salzsieder Ethan <824658@stma.k12.mn.us>
2025-09-29 13:41:20 -0500

    data conversion

---
+++ Practice/data_conversions.py
@@ -0,0 +1,43 @@
+a = 115         #int -> string
+a = str(a)
+print(a)
+
+b = 3.14        #float -> string
+b = str(b)
+print(b)
+
+c = "68"        #string -> int
+c = int(c)
+print(c)
+
+d = "True"      #string -> boolean
+d = bool(d)
+print(d)
+
+e = True        #boolean -> string
+e = str(e)
+print(e)
+
+f = False       #boolean -> string
+f = str(f)
+print(f)
+
+g = '10110111'  #byte -> int
+g = int(g)
+print(g)
+
+h = "2.54"      #string -> float
+h = float(h)
+print(h)
+
+i = 100         #int -> float
+i = float(i)
+print(i)
+
+j = 10.0        #float -> int
+j = int(j)
+print(j)
+
+k = 254         #int -> byte
+k = bin(k)
+print(k)
