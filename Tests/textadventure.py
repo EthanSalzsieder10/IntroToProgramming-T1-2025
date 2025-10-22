7c51c4b18cf13aead81b2526ee803c00e193d407
Salzsieder Ethan <824658@stma.k12.mn.us>
2025-10-22 13:26:27 -0500

    Text Adventure Work

--- Tests/textadventure.py
+++ Tests/textadventure.py
@@ -1,40 +1,38 @@
 # Setup
-mode = None
 def game_mode():
     print()
     print("What game mode do you want to play?")
     print("1. Trophy Road")
     print("2. Classic 1v1")
     print("3. Classic 2v2")
+    print("4. None, play Clash of Clans")
     choice = input(">>>")
     print()
-    global mode
     if choice == "1":
-        mode = 1
-        print("You load into a trophy road game. Your opponent's tower level is two above yours and they are already spamming the goblin 'mimimimimi' emote.")
+        tr_first_play()
     elif choice == "2":
-        mode = 2
         print("You load into a Classic 1v1. Your opponent sends a 'good luck!' emote. You don't know if that is friendly or a warning.")
     elif choice == "3":
-        mode = 3
         print("You load into a 2v2. Your teammate immediately leaves.")
+    elif choice == "4":
+        ending_1()
     else:
-        print("Invalid Input. Must be a number 1-3.")
+        print("Invalid Input. Must be a number 1-4.")
         game_mode()
-
-def first_play():
+def tr_first_play():
+    print("You load into a trophy road game. Your opponent's tower level is two above yours and they are already spamming the goblin 'mimimimimi' emote.")
     print()
-    print("The first 10 seconds pass and no one has played a card. You are leaking elixer, as is the enemy. What do you do?")
-    print("1. Push valkyrie and prince on the bridge")
+    print("The first 10 seconds pass and no one has played a card. You are leaking elixer, as is the enemy. Your current cards are Valkyrie, Prince, Fireball, and Skarmy. What do you do?")
+    print("1. Push Valkyrie and Prince on the bridge")
     print("2. Wait for the opponent to play first")
     print("3. Fireball the king tower")
-    print("4. Split skarmy in the back")
+    print("4. Split Skarmy in the back")
     choice = input(">>>")
     print()
     if choice == "1":
-        print("You push a valk and prince on the bridge. Your opponent tries to counter with a skarmy, but the valkyrie makes quick work of them and you get juicy tower damage.")
+        tr_prince_valk()
     elif choice == "2":
-        print("Your opponent plays lumberloon.")
+        print("Your opponent ")
     elif choice == "3":
         print("You fireball the king tower. It's tower damage, right?")
     elif choice == "4":
@@ -42,13 +40,14 @@ def first_play():
     else:
         print("Invalid Input. Must be a number 1-4.")
         encounter()
-    
-def encounter():
+def tr_prince_valk():
+    print("You push a Valk and Prince on the bridge. Your opponent plays a Pekka, which mostly defends, but you get a few hundred damage on the tower.")
     print()
     print("")
     print("1. ")
     print("2. ")
     print("3. ")
+    print("4. ")
     choice = input(">>>")
     print()
     if choice == "1":
@@ -57,14 +56,47 @@ def encounter():
         print("")
     elif choice == "3":
         print("")
+    elif choice == "4":
+        print("")
     else:
         print("")
         encounter()
 
+def encounter():
+    print()
+    print("")
+    print("1. ")
+    print("2. ")
+    print("3. ")
+    print("4. ")
+    choice = input(">>>")
+    print()
+    if choice == "1":
+        print("")
+    elif choice == "2":
+        print("")
+    elif choice == "3":
+        print("")
+    elif choice == "4":
+        print("")
+    else:
+        print("")
+        encounter()
+def ending_1():
+    print()
+    print("You played Clash of Clans.")
+    print("Ending 1 of 5: Worst Ending")
+    print()
+def ending_2():
+    print()
+    print("You stop playing Clash indefinitely. You become much happier as a person and you go on to get married to your soulmate and become a trillionaire with 3 kids.")
+    print("Ending 2 of 5: Good Ending")
+    print()
 # Gameplay
 print()
 print("You are a high school boy who is hopelessly addicted to Clash Royale. You pull out your phone in the middle of math class because you physically can't live without it.")
 game_mode()
-first_play()
 
-# Deck: Prince, valk, fireball, evo inferno dragon, baby dragon, log, cannon, skarmy
+# Deck: Prince, Evo Valk, Fireball, Evo Inferno Dragon, Baby Dragon, Log, Cannon, Skarmy
+# TR Op Deck: Pekka, Skarmy, Evo Mega Knight, Tesla Tower, Rocket, Log, Evo Goblin Barrel, Freeze
+# C1v1 Op Deck: Hog, Musketeer, Ice Golem, Fireball, Ice Spirit, Fire Spirit, Skeletons, Cannon
