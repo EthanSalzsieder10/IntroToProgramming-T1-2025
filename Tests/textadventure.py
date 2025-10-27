55988fdaf68794b396664dcda1b91ae304da9635
EthanSalzsieder10 <824658@mystma.org>
2025-10-26 20:39:18 -0500

    Update textadventure.py

--- Tests/textadventure.py
+++ Tests/textadventure.py
@@ -66,7 +66,8 @@ def fire_king():
     global op_tower
     king_activate = True
     op_tower = op_tower - 150
-    print("You strategically place your fireball to hit the King and Princess tower. You're a genius.\n\nYour Opponent sends a laughing emote and then plays a Mega Knight. What do you do?")
+    print("You strategically place your fireball to hit the King and Princess tower. You're a genius.\n")
+    print("Your Opponent sends a laughing emote and then plays a Mega Knight. What do you do?")
     print("1. Place a cannon to distract")
     print("2. Play Inferno Dragon on the Mega Knight")
     print("3. Push Skarmy on the other lane")
@@ -75,11 +76,11 @@ def fire_king():
     print()
     if choice == "1":
         print("You place a cannon to distract the Mega Knight.")
-        cannon()
+        mk_cannon()
     elif choice == "2":
-        print("")
+        mk_inferno()
     elif choice == "3":
-        print("")
+        mk_skarmy()
     elif choice == "4":
         global doubt
         doubt = doubt + 1
@@ -87,9 +88,10 @@ def fire_king():
         cannon()
     else:
         print("Invalid Input. Must be a number 1-4.")
-        encounter()
-def cannon():
+        fire_king()
+def mk_cannon():
     global my_tower
+    global op_tower
     my_tower = my_tower - 500
     print("\nThe cannon distracts the Mega Knight long enough for the tower to only take a few hits. You suspect the opponent has no elixer. What do you do?")
     print("1. Push with Inferno Dragon")
@@ -99,28 +101,124 @@ def cannon():
     choice = input(">>>")
     print()
     if choice == "1":
-        inferno_dragon()
+        if king_activate == True:
+            print("It would've worked, but you activated the King Tower by fireballing it so you only get chip damage.")
+            op_tower = op_tower - 500
+            final_minute()
+        else:
+            print("Your opponent can't defend the dragon and you get a lot of damage on the tower.")
+            op_tower = op_tower - 2000
+            final_minute()
     elif choice == "2":
-        print("He didn't have a lot of elixer, but he had a little. Your push gets ruined by a single log.")
+        print("He didn't have a lot of elixer, but he had a little. Your push gets ruined by a single log.\n")
+        final_minute()
     elif choice == "3":
-        print("Prince is the best and most balanced card. Your opponent's tower effectively never existed.")
+        prince_push()
     elif choice == "4":
-        print("As you gain more elixer, so does your opponent. He rockets your Tower.")
+        tower_rocket()
     else:
         print("Invalid Input. Must be a number 1-4.")
-        cannon()
-def inferno_dragon():
+        mk_cannon()
+def mk_inferno():
+    global my_tower
+    my_tower = my_tower - 500
+    print("The dragon kills the Mega Knight in a decently short time, but there was nothing distracting it, so it gets a few hits on the tower.\n")
+    print("Mega Knight costs 7 elixer, so you should be able to play something.")
+    print("1. Push with Prince")
+    print("2. Push with Skarmy")
+    print("4. Wait to build elixer")
+    choice = input(">>>")
+    print()
+    if choice == "1":
+        prince_push()
+    elif choice == "2":
+        print("He didn't have a lot of elixer, but he had a little. Your push gets ruined by a single log.\n")
+        final_minute()
+    elif choice == "3":
+        tower_rocket()
+    else:
+        print("Invalid Input. Must be a number 1-3.")
+        mk_inferno()
+def mk_skarmy():
+    global my_tower
+    my_tower = my_tower - 2500
     global op_tower
-    global king_activate
-    if king_activate == True:
-        print("It would've worked, but you activated the King Tower by fireballing it so you only get chip damage.")
-        op_tower = op_tower - 500
+    op_tower = op_tower - 2
+    print("Why? You get like 2 damage on the opponents tower, and he gets 2500 on yours.\n")
+    print("What do you do now?")
+    print("1. Push with Inferno Dragon")
+    print("2. Push with Prince")
+    print("3. Spam the 6 7 emote and do nothing else.")
+    choice = input(">>>")
+    print()
+    if choice == "1":
+        if king_activate == True:
+            print("It would've worked, but you activated the King Tower by fireballing it so you only get chip damage.")
+            op_tower = op_tower - 500
+            final_minute()
+        else:
+            print("Your opponent can't defend the dragon and you get a lot of damage on the tower.")
+            op_tower = op_tower - 2000
+            final_minute()
+    elif choice == "2":
+        prince_push()
+    elif choice == "3":
+        secret_ending_2()
+    else:
+        print("Invalid Input. Must be a number 1-4.")
+        mk_skarmy()
+def prince_push():
+    global op_tower
+    op_tower = op_tower - 2000
+    global my_tower
+    print("Prince is the best and most balanced card. Your opponent's tower takes massive damage.\n")
+    print("Your opponent tries to get you back with a goblin barrel. You have almost no elixer. What do you do?")
+    print("1. Play Log on the goblins")
+    print("2. Let the tower take care of the goblins")
+    choice = input(">>>")
+    print()
+    if choice == "1":
+        print("The log takes the goblins out quickly. They get no damage on the tower.")
+        final_minute()
+    elif choice == "2":
+        print("Your opponent freezes the tower and the goblins get a lot of damage.")
+        my_tower = my_tower - 500
         final_minute()
     else:
-        print("Your opponent can't defend the dragon and you get a lot of damage on the tower.")
-        op_tower = op_tower - 2000
+        print("Invalid Input. Must be a number 1-2.")
+        prince_push()
+def tower_rocket():
+    global my_tower
+    global op_tower
+    my_tower = my_tower - 600
+    print("As you build more elixer, so does your opponent. He rockets your tower\n")
+    print("Rocket costs 6 elixer, so he has to be low. What do you do?")
+    print("1. Push with Baby Dragon")
+    print("2. Push with skarmy")
+    print("3. Fireball the towers")
+    print("4. Wait for more elixer again")
+    choice = input(">>>")
+    print()
+    if choice == "1":
+        print("Your opponent can't defend, good play.")
+        op_tower = op_tower - 1000
         final_minute()
+    elif choice == "2":
+        print("Your opponent plays log. Why do people have swarm decks?")
+        final_minute()
+    elif choice == "3":
+        print("You know, you should save spells for when your opponent can actually defend, but okay.")
+        op_tower = op_tower - 200
+        final_minute()
+    elif choice == "4":
+        print("Your opponent mirrors rocket on the tower. Just play a card, you coward.")
+        my_tower = my_tower - 600
+        final_minute()
+    else:
+        print("Invalid Input. Must be a number 1-4.")
+        tower_rocket()
 def final_minute():
+    global op_tower
     print("\nYou enter the last minute of the game, and 2x elixer activates. No towers have been taken yet. What do you do?")
     print("1. Push with Valkyrie and Prince")
     print("2. Spell the tower")
@@ -130,8 +228,26 @@ def final_minute():
     print()
     if choice == "1":
         print("You push Valkyrie and Prince. Your opponent can only stall with a tesla tower, and takes some good damage.")
+        op_tower = op_tower - 500
+        if op_tower <= 0 and doubt > 0:
+            print("And that's all the damage you need to get the tower. Your opponent tries to spam on the bridge, but you defend enough to outlast the timer.")
+            ending_4()
+        elif op_tower <= 0:
+            print("And that's all the damage you need to get the tower. Your opponent tries to spam on the bridge, but you defend enough to outlast the timer.")
+            ending_5()
+        else:
+            universal_final_play()
     elif choice == "2":
         print("You spell the tower. It gets a little damage.")
+        op_tower = op_tower - 200
+        if op_tower <= 0 and doubt > 0:
+            print("And that's all the damage you need to get the tower. Your opponent tries to spam on the bridge, but you defend enough to outlast the timer.")
+            ending_4()
+        elif op_tower <= 0:
+            print("And that's all the damage you need to get the tower. Your opponent tries to spam on the bridge, but you defend enough to outlast the timer.")
+            ending_5()
+        else:
+            universal_final_play()
     elif choice == "3":
         print("You decide its better to wait for your opponent to plays first so you can properly counter.")
         final_wait()
@@ -160,9 +276,9 @@ def final_wait():
         print("You place a cannon to distract. It takes out the Mega Knight, but the Pekka still does massive damage to your tower")
         my_tower = my_tower - 2000
         if my_tower <= 0 and doubt > 0:
-            print("The Pekka takes your tower and your opponent sends a chicken emote.")
+            print("The Pekka takes your tower and your opponent sends a chicken emote.\n")
         elif my_tower <= 0:
-            print("The Pekka takes your tower and your opponent sends a chicken emote.")
+            print("The Pekka takes your tower and your opponent sends a chicken emote.\n")
         else:
             universal_final_play()
     elif choice == "3":
@@ -183,14 +299,14 @@ def final_wait():
 def universal_final_play():
     global op_tower
     global my_tower
-    print("\nIt's overtime now. You feel this game has lasted forever. You could make a text adventure out of this. You've lasted this long, you must win. You will win. What do you do?")
+    print("\nIt's overtime now. You feel like this game has lasted forever. If you lose this game, you might consider thinking about deleting the game. What do you do?")
     print("1. Push with Prince and Valkyrie")
     print("2. Push with Baby Dragon and Inferno Dragon")
-    print("3. Push with Skarmy and freeze on the tower.")
-    print("4. Place skarmy in the back and cannon in the center for defense.")
+    print("3. Push with Skarmy and freeze on the tower")
+    print("4. Place skarmy in the back and cannon in the center for defense")
     if doubt == 3:
         print("-OR-")
-        print("5. Delete Clash.")
+        print("5. Delete Clash and end the pain")
     choice = input(">>>")
     print()
     if choice == "1":
@@ -230,7 +346,7 @@ def universal_final_play():
         print("Your Opponent spams in one lane to get a quick win, but your defense tanks most of the damage.\n")
         my_tower = my_tower - 500
         if my_tower <= 0:
-            print("But most doesn't cut it. That little bit of damage is all he needs.")
+            print("But most doesn't cut it. That little bit of damage is all he needs.\n")
             if doubt > 0:
                 ending_2()
                 return
@@ -293,35 +409,9 @@ def ending_5():
 def secret_ending():
     print("You decide that is enough screwing around with your life. You delete Clash and you invest in your future. You grow up to marry your soulmate and have 3 children who live with you in your mansion.")
     print("Secret: Best Ending\n")
+def secret_ending_2():
+    print("Siiix Seeeveeeeen. That's a real emote you can get in the game now.")
+    print("Ending 6 of 7: 6 7\n")
 
 print("\nYou are a high school boy who is hopelessly addicted to Clash Royale. You pull out your phone in the middle of math class because you physically can't live without it.")
 start()
-import turtle
-t = turtle.Turtle()
-t.speed(2.5)
-t.shape("turtle")
-t.penup()
-t.goto(-175, 100)
-t.pendown()
-t.goto(-75, 100)
-t.goto(-175, 100)
-t.goto(-175, 0)
-t.goto(-75, 0)
-t.goto(-175, 0)
-t.goto(-175, -100)
-t.goto(-75, -100)
-t.penup()
-t.goto(-50, -100)
-t.pendown()
-t.goto(-50, 100)
-t.goto(50, -100)
-t.goto(50, 100)
-t.penup()
-t.goto(75, 100)
-t.pendown()
-t.goto(75, -100)
-t.goto(175, -50)
-t.goto(175, 50)
-t.goto(75, 100)
-# Deck: Prince, Evo Valk, Fireball, Evo Inferno Dragon, Baby Dragon, Log, Cannon, Skarmy
-# TR Op Deck: Pekka, Skarmy, Evo Mega Knight, Tesla Tower, Rocket, Log, Evo Goblin Barrel, Freeze
