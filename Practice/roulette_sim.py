0031a05a78023b74b83303d26e81d026bdfaecdc
EthanSalzsieder10 <824658@mystma.org>
2025-10-30 21:35:18 -0500

    gambling

---
+++ Practice/roulette_sim.py
@@ -0,0 +1,8 @@
+money = 1000
+def roulette():
+  print("What do you want to bet on?")
+  print("1. Color")
+  print("2. Even/Odd")
+  print("3. High/Low")
+  print("4. Single Number")
+  choice = input(">>>")
